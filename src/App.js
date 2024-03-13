3ee329bd4154d756195312e8fec1a93346462526
Kapadi-Hardik <kapadihardik@gmail.com>
2024-03-13 12:07:25 +0530

    use map and var in app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+let a = ['01','02','03','04','05']
+let title = "Heading"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      {
+       a.map((i)=>{
+         return <p>{i}</p>
+        })
+      }
+    
     </div>
   );
 }
